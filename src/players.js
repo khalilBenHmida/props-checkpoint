d934a243a92de2c3da009e08b1e956249e8c8b19
khalilBenHmida <khalilbusiness1000@gmail.com>
2024-02-13 15:49:57 +0100

    first commit

---
+++ src/players.js
@@ -0,0 +1,23 @@
+// src/players.js
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentinian",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://example.com/messi.jpg",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl: "https://example.com/ronaldo.jpg",
+    },
+ 
+  ];
+  
+  export default players;
+  
